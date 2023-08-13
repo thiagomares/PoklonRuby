5be7457309cafbdd82c0bfe7035bede6d0302cf8
thiagomares <thiago.mares4@gmail.com>
2023-08-13 18:37:08 -0300

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :usuarios
   resources :enderecousuarios
   resources :registro_doacos
+
+  root to: 'welcome#index'
+  # aqui estamos definindo nossa pagina como a principal na aplicação
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
